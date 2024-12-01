36054cc10624c30565b3b69509f090175cb8da6d
lukmanhakim141995 <lukmanpegongsoran@gmail.com>
2024-12-02 01:47:16 +0700

    updates

--- config/db.js
+++ config/db.js
@@ -1,16 +1,16 @@
 const { Pool } = require('pg');
 
-// Gunakan variabel lingkungan DATABASE_URL yang disediakan oleh Railway
+// Gunakan DATABASE_URL yang disediakan oleh Railway
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL, // Menggunakan DATABASE_URL dari Railway
+    connectionString: process.env.DATABASE_URL,  // Menggunakan URL koneksi yang diberikan oleh Railway
     ssl: {
-        rejectUnauthorized: false, // Pastikan koneksi aman (SSL) ke database PostgreSQL Railway
+        rejectUnauthorized: false,  // SSL harus diaktifkan untuk koneksi ke database di Railway
     },
 });
 
 const testConnection = async () => {
     try {
-        const res = await pool.query('SELECT NOW()'); // Cek waktu di server database
+        const res = await pool.query('SELECT NOW()'); // Memeriksa waktu di server database
         console.log('Koneksi berhasil! Waktu di server database:', res.rows[0].now);
     } catch (err) {
         console.error('Koneksi gagal:', err.message);
