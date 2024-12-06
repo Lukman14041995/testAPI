23fd392c826a48425c85d7c73eab7fb7028099d4
lukmanhakim141995 <lukmanpegongsoran@gmail.com>
2024-12-06 11:21:50 +0700

    updt

--- config/db.js
+++ config/db.js
@@ -1,23 +1,23 @@
-require('dotenv').config();  // Menambahkan baris ini untuk memuat file .env
-
 const { Pool } = require('pg');
+require('dotenv').config(); // Memuat variabel dari file .env
 
-// Menggunakan DATABASE_URL dari environment variables
+// Membuat pool koneksi dengan DATABASE_URL yang ada di file .env
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,  // Mengambil nilai dari DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
     ssl: {
-        rejectUnauthorized: false,  // Agar koneksi SSL tetap diterima
+        rejectUnauthorized: false, // Menghindari masalah dengan sertifikat SSL yang tidak terverifikasi
     },
 });
 
 // Fungsi untuk menguji koneksi
 const testConnection = async () => {
     try {
-        const res = await pool.query('SELECT NOW()');  // Mengeksekusi query
+        const res = await pool.query('SELECT NOW()'); // Mengeksekusi query
         console.log('Koneksi berhasil! Waktu di server database:', res.rows[0].now);
     } catch (err) {
         console.error('Koneksi gagal:', err.message);
     }
 };
 
-testConnection();  // Memanggil fungsi untuk menguji koneksi
+// Mengekspor pool dan fungsi testConnection
+module.exports = { pool, testConnection };
