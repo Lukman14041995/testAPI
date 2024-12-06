8ad20142766f29a904b3090a38c01536e59d5381
lukmanhakim141995 <lukmanpegongsoran@gmail.com>
2024-12-06 11:04:11 +0700

    updates

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use('/api', ppobRoutes);
 app.use('/api', balanceRoutes);
 
 // Set port and start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
